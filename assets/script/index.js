518879f12200434a2acf2b52ee7977dcae8370d1
Thelma-Dev <osifoaisosa@gmail.com>
2022-12-12 12:59:17 -0600

    Add map

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,16 +1,76 @@
 'use strict';
 
-// Utility functions
-function onEvent(event, selector, callback) {
-    return selector.addEventListener(event, callback);
+const track = document.querySelector('.track');
+const mapStructure = document.getElementById('map');
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidGhlbG1hLWRldiIsImEiOiJjbGJncnJqc2wwaXhjM29xd2liMXYzbmE4In0.c2LzFGTr8v0YUQlSfSe3mQ';
+
+function getLocation(position) {
+    const { latitude, longitude } = position.coords;
+    setUpMap([-97.19318,49.81453]);
 }
 
-function select(selector, parent = document) {
-    return parent.querySelector(selector);
+function errorHandler() {
+    setUpMap([-97.19318,49.81453])
 }
 
-function sleep(duration) {
-    return new Promise(resolve => {
-        setTimeout(resolve, duration);
-    })
+const options = {
+    enableHighAccuracy: true
 }
+
+const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-97.19318,49.81453]
+        },
+        properties: {
+          title: 'Location',
+          description: 'MITT Henlow Campus'
+        }
+      },
+    ]
+};
+
+
+function setUpMap(center) {
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center,
+        zoom: 15,
+        attributionControl: false,
+        Marker: false
+    });
+
+    map.addControl(new mapboxgl.NavigationControl());
+    for (const feature of geojson.features) {
+
+        const el = document.createElement('div');
+        el.classList.add('marker');
+         
+        new mapboxgl.Marker(el)
+            .setLngLat(feature.geometry.coordinates)
+            .setPopup(
+                new mapboxgl.Popup({ offset: 25 }) // add popups
+                    .setHTML(
+                        `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+                    )
+                )
+        .addTo(map);
+    }
+}
+
+// Event listeners
+
+track.addEventListener('click', () => { 
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getLocation, errorHandler, options);
+    } else {
+        console.log('Geolocation is not supported by your browser');
+    }
+})
